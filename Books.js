d3f7b24bd8aa924bd76ca896117ab717a3a32f40
Shankar <shankar@kali.kali>
2023-09-20 00:48:32 +0530

    update

--- Books.js
+++ Books.js
@@ -1,33 +1,8 @@
 const bookData = [
   {
-    title: "All Demo Titles",
-    author: "All Demo author",
-    ISBN: "978074327353463"
-  },
-  {
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     ISBN: "9780743273565",
-  },
-  {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    ISBN: "9780060935467",
-  },
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    ISBN: "9780743273565",
-  },
-  {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    ISBN: "9780061120084",
-  },
-  {
-    title: "1984",
-    author: "George Orwell",
-    ISBN: "9780451524935",
   },
   {
     title: "Pride and Prejudice",
